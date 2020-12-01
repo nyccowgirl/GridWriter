78a579b5f5e9106d7a721ad92c318821ad65f50a
nyccowgirl <nyc_cowgirl@hotmail.com>
2020-12-01 11:27:46 -0600

    Initial commit

---
+++ src/com/company/GridWriterProgram.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class GridWriterProgram {
+
+    public static void main(String[] args) {
+        GridWriter gw = new GridWriter(40, 50);
+
+        gw.add(new MyCircle(10, 10, 9));
+        gw.add(new MyCircle(25, 20, 12));
+        gw.add(new MyCircle(25, 20, 5));
+
+        gw.add(new MyRectangle(25, 25, 20, 15));
+        gw.add(new MyRectangle(5, 5, 3, 4));
+        gw.add(new MyRectangle(40, 0, 10, 10));
+
+        gw.add(new MySquare(5, 25, 3));
+        gw.add(new MySquare(10, 30, 7));
+
+        gw.display();
+    }
+}
---
+++ src/com/company/MySquare.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/*
+Trang Hoang
+CS111B - Assignment 6B
+ */
+
+public class MySquare extends GridItem{
+
+    private int side;
+
+    public MySquare(int xValue, int yValue, int s) {
+        x = xValue;
+        y = yValue;
+        side = s;
+    }
+
+    public double getArea() {
+        return Math.pow(side, 2.0);
+    }
+
+    public boolean containsPoint(int xValue, int yValue) {
+        return	xValue >= x &&
+                xValue <= x + side &&
+                yValue >= y &&
+                yValue <= y + side;
+    }
+}
